23907f3f76b88a89b6233bd3fb816a9aefee57d2
Eric Foard <foard265@gmail.com>
2018-08-29 18:40:00 -0700

    allow user to select plane

--- js/components/ARView.js
+++ js/components/ARView.js
@@ -3,14 +3,23 @@ import { connect } from 'react-redux';
 import {
   ViroARSceneNavigator,
   ViroARScene,
+  ViroARPlaneSelector,
   ViroImage
 } from 'react-viro';
 import { API_KEY } from '../../env.js';
+
+const MIN_PLANE_DIMENSION = 0.05;
 
 // TODO: this will not be needed when store dimensions in meters
 const formatDimension = dim => {
   const INCH_TO_METER = 0.0254; // inches => meters
   return dim * INCH_TO_METER;
+};
+
+const pointCloudOpts = {
+  imageSource: require("../res/pointCloudPoint.png"),
+  imageScale: [.02, .02, .02],
+  maxPoints: 100
 };
 
 class ARView extends Component {
@@ -30,14 +39,25 @@ class ARView extends Component {
 
   renderScene() {
     const { height, width, image } = this.props.product;
+    const widthFormatted = formatDimension(width);
+    const heightFormatted = formatDimension(height);
     return (
-      <ViroARScene>
-        <ViroImage
-          source={{ uri: image }}
-          height={formatDimension(height)}
-          width={formatDimension(width)}
+      <ViroARScene
+        displayPointCloud={pointCloudOpts}
+        anchorDetectionTypes={'PlanesVertical'}>
+        <ViroARPlaneSelector
+        // minHeight={heightFormatted}
+        // minWidth={widthFormatted}
+        alignment={'Vertical'}
+        onPlaneSelected={() => console.warn('plane selected!!!!')}>
+          <ViroImage
+            source={{ uri: image }}
+            height={heightFormatted}
+            width={widthFormatted}
+            rotation={[270,0,0]}
           // position={[0,0,-1]}
-        />
+          />
+        </ViroARPlaneSelector>
       </ViroARScene>
     );
   }
