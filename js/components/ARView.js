5cc5ef5c1a4741cd995aaf177d1aafce65ab656f
Eric Foard <foard265@gmail.com>
2018-09-03 08:58:13 -0700

    drag and release line to place image

--- js/components/ARView.js
+++ js/components/ARView.js
@@ -3,12 +3,18 @@ import { connect } from 'react-redux';
 import {
   ViroARSceneNavigator,
   ViroARScene,
-  ViroARPlaneSelector,
-  ViroImage
+  ViroARPlane,
+  ViroImage,
+  ViroBox,
+  ViroNode
 } from 'react-viro';
 import { API_KEY } from '../../env.js';
 
 const MIN_PLANE_DIMENSION = 0.05;
+const ROTATION_START = 1, ROTATION_END = 3;
+let currRotation = null;
+let wallDist = 0;
+let placementTimeout = null;
 
 // TODO: this will not be needed when store dimensions in meters
 const formatDimension = dim => {
@@ -25,7 +31,17 @@ const pointCloudOpts = {
 class ARView extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      anchorPt: [0, 0, 0],
+      showPointClound: true,
+      viroNode: null,
+      showImage: false
+    }
+
     this.renderScene = this.renderScene.bind(this);
+    this.handleAnchorFound = this.handleAnchorFound.bind(this);
+    this.handleRotate = this.handleRotate.bind(this);
+    this.handleDrag = this.handleDrag.bind(this);
   }
 
   render() {
@@ -38,28 +54,79 @@ class ARView extends Component {
   }
 
   renderScene() {
+    // setTimeout(() => { this.setState({ showImage: true }) }, 15000)
     const { height, width, image } = this.props.product;
     const widthFormatted = formatDimension(width);
     const heightFormatted = formatDimension(height);
     return (
       <ViroARScene
-        displayPointCloud={pointCloudOpts}
-        anchorDetectionTypes={'PlanesVertical'}>
-        <ViroARPlaneSelector
-        // minHeight={heightFormatted}
-        // minWidth={widthFormatted}
-        alignment={'Vertical'}
-        onPlaneSelected={() => console.warn('plane selected!!!!')}>
-          <ViroImage
-            source={{ uri: image }}
-            height={heightFormatted}
-            width={widthFormatted}
-            rotation={[270,0,0]}
-          // position={[0,0,-1]}
-          />
-        </ViroARPlaneSelector>
+        displayPointCloud={this.state.showPointClound && pointCloudOpts}
+        anchorDetectionTypes={'PlanesHorizontal'}
+        onRotate={this.handleRotate}
+      >
+        <ViroARPlane
+          minHeight={MIN_PLANE_DIMENSION}
+          minWidth={MIN_PLANE_DIMENSION}
+          alignment={'Horizontal'}
+          onAnchorFound={this.handleAnchorFound}>
+          <ViroNode
+            ref={c => this.state.viroNode = c}
+            onDrag={this.handleDrag}
+            dragType='FixedToPlane'
+            dragPlane={{
+              planePoint: this.state.anchorPt,
+              planeNormal: [0, 1, 0],
+              maxDistance: 5
+            }}>
+            <ViroBox
+              height={0.01}
+              length={0.01}
+              width={5}
+              visible={!this.state.showImage}
+            />
+            <ViroImage
+              source={{ uri: image }}
+              height={heightFormatted}
+              width={widthFormatted}
+              position={[0, 1.5, 0]}
+              visible={this.state.showImage}
+            />
+          </ViroNode>
+        </ViroARPlane>
       </ViroARScene>
     );
+  }
+
+  handleDrag(pos) {
+    wallDist = pos[2];
+    if (placementTimeout) {
+      clearTimeout(placementTimeout);
+    }
+    placementTimeout = setTimeout(() => { this.setState({ showImage: true }) }, 250);
+  }
+
+  handleRotate(rotateState, rotationFactor) {
+    switch (rotateState) {
+      case ROTATION_START:
+        break;
+      case ROTATION_END:
+        break;
+      default: // otherwise, rotation in progress
+        this.state.viroNode.setNativeProps({
+          rotation: [currRotation[0], currRotation[1] + rotationFactor, currRotation[2]]
+        });
+        break;
+    }
+  }
+
+  handleAnchorFound({ position, rotation }) {
+    this.setState({
+      anchorPt: position,
+      showPointClound: false
+    });
+    let y = rotation[1];
+    currRotation = [0, y, 0];
+    wallDist = position[2];
   }
 }
 
